21c4fc170e635d0e014065c64b42ac92914c51bb
Kim Hyun Jae <namro7633@gmail.com>
2023-07-09 15:59:33 +0900

    Echarts 라이브러리 추가

--- src/views/showSales.vue
+++ src/views/showSales.vue
@@ -1,14 +1,23 @@
 <template>
   <v-container>
     <h1>매출관리화면</h1>
+   <v-chart></v-chart>
   </v-container>
 
 </template>
 
 <script>
+import Vue from 'vue'
+import Echart from 'vue-echarts'
+import 'echarts-gl'
+
 export default {
+  components:{
+    v-chart : Echart
+  }
     name:'showSales'
 }
+
 </script>
 
 <style>
